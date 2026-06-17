2eb1341fcae701477812d05b1f5b5d33dbc16088
PassantGamal <basantgamal070@gmail.com>
2026-06-17 21:29:12 +0300

    AddEmployeeController

---
+++ HumaVolveAPI/Controllers/EmployeesController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HumaVolveAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EmployeesController : ControllerBase
+    {
+        private static List<string> employees = new List<string>
+        {
+            "Ahmed", "Sara", "Ali"
+        };
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Ok(employees);
+        }
+
+        [HttpPost]
+        public IActionResult AddEmployee(string name)
+        {
+            employees.Add(name);
+            return Ok(employees);
+        }
+    }
+}
